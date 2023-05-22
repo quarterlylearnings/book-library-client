5da95a9fa85515c55ab6b1299de876a6ecbb64b6
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-22 14:51:58 -0400

    Adds new library form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LibraryList from './LibraryList';
 import ShelfList from './ShelfList';
 import BookList from './BookList';
+import LibraryForm from './LibraryForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LibraryList />} />
+        <Route path="/libraries/new" element={<LibraryForm />} />
         <Route path="/libraries/:libraryId/shelves" element={<ShelfList />} />
         <Route path="/shelves/:shelfId/books" element={<BookList />} />
       </Routes>
