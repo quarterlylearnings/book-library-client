fd8ca380fef7c90c813af2a787c4a13a0726efd5
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-24 13:59:06 -0400

    Adds button for creating new shelf

--- src/ShelfList.js
+++ src/ShelfList.js
@@ -1,10 +1,15 @@
 import { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { ListGroup } from 'react-bootstrap';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Button, Col, Container, ListGroup, Row, Stack } from 'react-bootstrap';
 
 function ShelfList() {
   const [shelves, setShelves] = useState([]);
   const { libraryId } = useParams();
+  const navigate = useNavigate();
+
+  const addShelf = () => {
+    navigate('/libraries/:libraryId/shelves/new');
+  }
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_BASE_URL}/libraries/${libraryId}/shelves`)
@@ -13,14 +18,24 @@ function ShelfList() {
   }, [libraryId]);
 
   return (
-    <ListGroup>
-      {shelves.map(shelf => (
-        <ListGroup.Item key={shelf.id}>
-          {shelf.genre}
-          <Link to={`/shelves/${shelf.id}/books`}>View Books</Link>
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
+    <Container>
+      <Stack gap={2}>
+        <ListGroup>
+          {shelves.map(shelf => (
+            <ListGroup.Item key={shelf.id}>
+              {shelf.genre}
+              <Link to={`/shelves/${shelf.id}/books`}>View Books</Link>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+        <Row>
+        <Col xs={{offset:10}}>
+
+          <Button onClick={addShelf}>+</Button>
+          </Col>
+        </Row>
+      </Stack>
+    </Container>
   );
 }
 
